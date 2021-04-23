df71623c5fe2ed991923160d095399aa86a5976f
ignocu-eke <zouyanpo19197165@126.com>
2021-04-23 11:17:10 +0800

    远程master 提交第一行代码

--- src/com/accp/demo/Test1.java
+++ src/com/accp/demo/Test1.java
@@ -2,6 +2,6 @@ package com.accp.demo;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("本地Master第一行代码");
+        System.out.println("远程origin/master第一行代码")
     }
 }
